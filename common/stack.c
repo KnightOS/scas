85d2dd76b12b57c85f4263cc8773ed116d560f9b
Miles Rout <miles.rout@gmail.com>
2014-11-03 17:24:45 +1300

    implement stack_t
    fixed declaration of stack_pop.
    added stack_shrink_to_fit, stack_peek.
    added STACK_GROWTH_RATE macro in stack.h.

---
+++ common/stack.c
@@ -0,0 +1,38 @@
+#include "stack.h"
+
+stack_t *create_stack() {
+	stack_t *stack = malloc(sizeof *stack);
+
+	stack->capacity = STACK_GROWTH_RATE;
+	stack->length = 0;
+	stack->items = calloc(STACK_GROWTH_RATE, sizeof(void*));
+
+	return stack;
+}
+
+void stack_free(stack_t *stack) {
+	free(stack->items);
+	free(stack);
+}
+
+void stack_push(stack_t *stack, void *item) {
+	if (stack->capacity <= stack->length) {
+		stack->capacity += STACK_GROWTH_RATE;
+		stack->items = realloc(stack->items, capacity);
+	}
+
+	stack->items[stack->length++] = item;
+}
+
+void *stack_pop(stack_t *stack) {
+	return stack->items[--stack->length];
+}
+
+void *stack_peek(stack_t *stack) {
+	return stack->items[stack->length - 1];
+}
+
+void stack_shrink_to_fit(stack_t *stack) {
+	stack->capacity = stack->length;
+	stack->items = realloc(stack->items, stack->capacity);
+}
