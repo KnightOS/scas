9b85533f4d8a36f5d4a32649a4397cc8a8a68600
Puck Meerburg <puck@puckipedia.nl>
2014-11-07 00:20:53 +0100

    Switch right and left operands in operators

--- common/operators.c
+++ common/operators.c
@@ -5,8 +5,8 @@
 #include "expression.h"
 
 uint64_t operator_add(stack_t *stack, int *error) {
-	expression_token_t *left = stack_pop(stack);
 	expression_token_t *right = stack_pop(stack);
+	expression_token_t *left = stack_pop(stack);
 	if (left == NULL || right == NULL) {
 		*error = EXPRESSION_BAD_SYNTAX;
 		return 0;
@@ -15,8 +15,8 @@ uint64_t operator_add(stack_t *stack, int *error) {
 }
 
 uint64_t operator_subtract(stack_t *stack, int *error) {
-	expression_token_t *left = stack_pop(stack);
 	expression_token_t *right = stack_pop(stack);
+	expression_token_t *left = stack_pop(stack);
 	if (left == NULL || right == NULL) {
 		*error = EXPRESSION_BAD_SYNTAX;
 		return 0;
@@ -25,8 +25,8 @@ uint64_t operator_subtract(stack_t *stack, int *error) {
 }
 
 uint64_t operator_multiply(stack_t *stack, int *error) {
-	expression_token_t *left = stack_pop(stack);
 	expression_token_t *right = stack_pop(stack);
+	expression_token_t *left = stack_pop(stack);
 	if (left == NULL || right == NULL) {
 		*error = EXPRESSION_BAD_SYNTAX;
 		return 0;
@@ -35,8 +35,8 @@ uint64_t operator_multiply(stack_t *stack, int *error) {
 }
 
 uint64_t operator_divide(stack_t *stack, int *error) {
-	expression_token_t *left = stack_pop(stack);
 	expression_token_t *right = stack_pop(stack);
+	expression_token_t *left = stack_pop(stack);
 	if (left == NULL || right == NULL) {
 		*error = EXPRESSION_BAD_SYNTAX;
 		return 0;
