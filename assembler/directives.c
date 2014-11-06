87a8bf5f717601fe25c48ff7440e497c9ed2fd4c
Drew DeVault <ddevault@hosting.com>
2014-11-06 09:59:56 -0700

    Implement directive matching

--- assembler/directives.c
+++ assembler/directives.c
@@ -1,29 +1,45 @@
 #include "directives.h"
 #include "errors.h"
 #include "expression.h"
+#include <strings.h>
+#include <ctype.h>
 #include <stdint.h>
 
 #define ERROR(ERROR_CODE, COLUMN) add_error(state.errors, ERROR_CODE, state.line_number, state.line, COLUMN, state.file_name);
 
 struct directive {
 	char *match;
-	int(*function)(struct assembler_state state, char *line);
+	int(*function)(struct assembler_state state, char **argc, int argv);
 };
 
-int handle_db(struct assembler_state state, char *line) {
-	return 0;
-}
-
-struct directive *find_directive(char *line) {
-	++line;
-	/* TODO: Binary search */
-	return NULL;
+int handle_db(struct assembler_state state, char **argc, int argv) {
+	return 1;
 }
 
 /* Keep this alphabetized */
 struct directive directives[] = {
 	{ "db", handle_db }
 };
+
+struct directive *find_directive(char *line) {
+	++line;
+	int whitespace = 0;
+	while (line[whitespace] && !isspace(line[whitespace++])); --whitespace;
+	int length = sizeof(directives) / sizeof(struct directive);
+	int min = 0, max = length - 1;
+	while (max >= min) {
+		int mid = (max - min) / 2;
+		int cmp = strncasecmp(directives[mid].match, line, whitespace);
+		if (cmp == 0) {
+			return &directives[mid];
+		} else if (cmp < 0) {
+			min = mid + 1;
+		} else {
+			max = mid - 1;
+		}
+	}
+	return NULL;
+}
 
 int try_handle_directive(struct assembler_state state, char **line) {
 	if (**line == '.' || **line == '#') {
@@ -32,7 +48,7 @@ int try_handle_directive(struct assembler_state state, char **line) {
 			ERROR(ERROR_INVALID_DIRECTIVE, state.column);
 			return 1;
 		}
-		return d->function(state, *line);
+		return d->function(state, NULL, 0);
 	}
 	return 0;
 }
