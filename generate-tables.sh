bcebf21f3e88ac46fe2e0ea4d9cf7d424f54cba0
Ken Peterson <arceusthe@gmail.com>
2018-11-10 15:56:49 -0800

    Fixed the changed files
    fixed

--- generate-tables.sh
+++ generate-tables.sh
@@ -9,10 +9,11 @@ else
 fi
 
 source_file1=$(cat "$1" | sed $sed_opt s/$/\\\\n\\\\/g)
+source_file1=${source_file1%?}
 source_file2=$(cat "$2" | sed $sed_opt s/$/\\\\n\\\\/g)
 mkdir -p $(dirname "$3")
 
-cat <<EOF > "$2"
+cat <<EOF > "$3"
 #include "generated.h"
 
 const char *z80_tab = "\
