d274848dabd366ed9ad0d33c1b48e83a89cb4ebf
Ken Peterson <arceusthe@gmail.com>
2018-11-09 21:03:17 -0800

    Add files via upload

--- generate-tables.sh
+++ generate-tables.sh
@@ -8,13 +8,16 @@ else
     sed_opt="-r"
 fi
 
-source_file=$(cat "$1" | sed $sed_opt s/$/\\\\n\\\\/g)
-mkdir -p $(dirname "$2")
+source_file1=$(cat "$1" | sed $sed_opt s/$/\\\\n\\\\/g)
+source_file2=$(cat "$2" | sed $sed_opt s/$/\\\\n\\\\/g)
+mkdir -p $(dirname "$3")
 
 cat <<EOF > "$2"
 #include "generated.h"
 
 const char *z80_tab = "\
-$source_file
+$source_file1"
+"\
+$source_file2
 ";
 EOF
