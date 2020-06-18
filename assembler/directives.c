c4439c9707bc14b51d9fdb0fa89cac57ff632b18
Noam Preil <noam@pixelhero.dev>
2020-06-17 23:30:02 -0400

    Fix macro parsing

--- assembler/directives.c
+++ assembler/directives.c
@@ -1151,10 +1151,14 @@ int handle_macro(struct assembler_state *state, char **argv, int argc) {
 				end = strchr(location, ')');
 			}
 
-			if (!end || end == location) {
+			if (!end) {
 				ERROR(ERROR_INVALID_DIRECTIVE, state->column, "unterminated parameter list");
 				return 1;
 				// TODO: Free everything
+			}
+			else if (end == location && end == ')') {
+    				// No parameters
+    				break;
 			}
 			char *parameter = malloc(end - location + 1);
 			strncpy(parameter, location, end - location);
