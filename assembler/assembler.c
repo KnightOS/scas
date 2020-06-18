b2f84c50467e6c69097814670a01f16ebd1bbed6
Noam Preil <pleasantatk@gmail.com>
2020-06-17 23:05:45 -0400

    Bugfixes

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -364,7 +364,7 @@ int try_match_instruction(struct assembler_state *state, char **_line) {
 					unresolved_sym->line = malloc(strlen(state->line) + 1);
 					strcpy(unresolved_sym->line,state->line);
 					const char *file_name = stack_peek(state->file_name_stack);
-					unresolved_sym->file_name = malloc(sizeof(file_name)+1);
+					unresolved_sym->file_name = malloc(strlen(file_name)+1);
 					strcpy(unresolved_sym->file_name,file_name);
 					list_add(state->object->unresolved,unresolved_sym);
 				}
