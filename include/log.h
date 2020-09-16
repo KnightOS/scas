466a661f37809a551ff5ea0549349fd7cd9b5d78
Noam Preil <noam@pixelhero.dev>
2020-09-16 10:55:20 -0400

    logging: cleanup

--- include/log.h
+++ include/log.h
@@ -1,19 +1,20 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <stdbool.h>
+
 typedef enum {
     L_SILENT = 0,
     L_ERROR = 1,
     L_INFO = 2,
     L_DEBUG = 3,
-} log_importance_t;
+} scas_log_importance_t;
 
-void init_log(int verbosity);
-void scas_log(int verbosity, char* format, ...);
+void scas_log_init(scas_log_importance_t verbosity);
+void scas_log(scas_log_importance_t verbosity, char* format, ...);
 void scas_abort(char* format, ...);
-void indent_log();
-void deindent_log();
-void enable_colors();
-void disable_colors();
+void scas_log_indent();
+void scas_log_deindent();
+void scas_log_set_colors(bool enabled);
 
 #endif
