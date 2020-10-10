c8c05cac708db268ee96cfd9e405748d59f01e13
Noam Preil <noam@pixelhero.dev>
2020-10-10 16:20:05 -0400

    Fix scas_read

--- common/io.c
+++ common/io.c
@@ -3,7 +3,7 @@
 
 void scas_read(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 	const unsigned read = fread(ptr, size, nmemb, stream);
-	if (read != nmemb * size) {
-		scas_abort("File read returned %d, %d expected", read, nmemb * size);
+	if (read != nmemb) {
+		scas_abort("File read returned %d, %d expected", read * size, nmemb * size);
 	}
 }
